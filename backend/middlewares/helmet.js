2c3914a3f30564bc7f85eed4a2d7ec8b57ac1986
cyrillgalkin <cyrillgalkin@gmail.com>
2023-05-11 17:46:22 +0300

    Fix helmet

--- backend/middlewares/helmet.js
+++ backend/middlewares/helmet.js
@@ -14,8 +14,6 @@ const helmetConfig = {
   ieNoOpen: true,
   /* Enable the X-Content-Type-Options header to prevent MIME type sniffing */
   noSniff: true,
-  /* Set the referrer policy to same-origin */
-  referrerPolicy: { policy: 'same-origin' },
   /* Enable the X-XSS-Protection header to enable the browser's built-in XSS protection */
   xssFilter: true,
   /* Disable the X-Powered-By header */
