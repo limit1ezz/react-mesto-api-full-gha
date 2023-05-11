6c95c98ae608cece9bf62196d7d7bcf6c642946a
cyrillgalkin <cyrillgalkin@gmail.com>
2023-05-11 17:39:03 +0300

    Fix cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -4,8 +4,12 @@ const corsOptions = {
     'https://practicum-cyrillgalkin.nomoredomains.monster',
     'http://www.practicum-cyrillgalkin.nomoredomains.monster',
     'https://www.practicum-cyrillgalkin.nomoredomains.monster',
+    'http://api.practicum-cyrilgalkin.nomoredomains.monster',
+    'https://api.practicum-cyrilgalkin.nomoredomains.monster',
+    'http://www.api.practicum-cyrilgalkin.nomoredomains.monster',
+    'https://www.api.practicum-cyrilgalkin.nomoredomains.monster',
   ],
-  methods: 'GET,POST,PUT,PATCH,DELETE',
+  methods: 'GET,POST,PUT,PATCH,DELETE,HEAD',
   allowedHeaders: 'Content-Type, Authorization',
 };
 
